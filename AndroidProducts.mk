9366c97c9bc2e36577f332019cbb1a2c5960173e
imannig <zahranansaria@gmail.com>
2022-06-24 11:20:39 +0700

    juice: Adapt tree to kasumi
    Signed-off-by: imannig <zahranansaria@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_juice.mk
+    $(LOCAL_DIR)/kasumi_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    kasumi_juice-user \
+    kasumi_juice-userdebug \
+    kasumi_juice-eng
